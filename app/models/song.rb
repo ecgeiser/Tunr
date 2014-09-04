feb90817c6d20cbeef33889de1ee63165ac57829
ecgeiser <erica.geiser@gmail.com>
2014-09-04 16:07:54 -0400

    added purchases model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
 	belongs_to :artist
 	has_many :purchases
+	has_many :users, through: :purchases
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :purchases
+	has_many :songs, through: :purchases
 	validates :email, presence: true
 	validates :password_digest, presence: true
 end
